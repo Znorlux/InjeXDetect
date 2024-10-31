b3be8bc0b88adecf0d06cf5b3a073812aeb17609
Znorlux <christianlombardi0414@gmail.com>
2024-10-31 00:41:16 -0500

    Añadido Ascii épico

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ device_info_str = lambda device_info: f"{device_info[ID_MODEL]} ({device_info[ID
 
 # Define the `on_connect` and `on_disconnect` callbacks
 def on_connect(device_id, device_info):
+
     print(f"Connected: {device_info_str(device_info)}")
-    # Execute the .exe file
-    exe_path = os.path.join(os.getcwd(), "keylog.exe")  # Cambia 'tu_archivo.exe' al nombre de tu archivo
+    exe_path = os.path.join(os.getcwd(), "keylog.exe")  
     print(f"Starting process: {exe_path}")
 
     try:
         process = subprocess.Popen(exe_path)
         print("Process started. Waiting for 15 seconds...")
 
-        # Esperar 15 segundos
+        # El programa esperará 15 segundos donde el keylogger obtendra los datos y formará el archivo CSV
         time.sleep(15)
 
         # Terminar el proceso
@@ -67,6 +67,20 @@ monitor = USBMonitor()
 monitor.start_monitoring(on_connect=on_connect, on_disconnect=on_disconnect)
 
 try:
+    print("""\
+          
+    8888888           d8b         Y88b   d88P 8888888b.           888                     888    
+      888             Y8P          Y88b d88P  888  "Y88b          888                     888    
+      888                           Y88o88P   888    888          888                     888    
+      888   88888b.  8888  .d88b.    Y888P    888    888  .d88b.  888888 .d88b.   .d8888b 888888 
+      888   888 "88b "888 d8P  Y8b   d888b    888    888 d8P  Y8b 888   d8P  Y8b d88P"    888    
+      888   888  888  888 88888888  d88888b   888    888 88888888 888   88888888 888      888    
+      888   888  888  888 Y8b.     d88P Y88b  888  .d88P Y8b.     Y88b. Y8b.     Y88b.    Y88b.  
+    8888888 888  888  888  "Y8888 d88P   Y88b 8888888P"   "Y8888   "Y888 "Y8888   "Y8888P  "Y888 
+                      888                                                                        
+                     d88P                                                                        
+                   888P"                                                                         
+          """)
     print("USB monitoring started. Waiting for devices to connect...")
     # Keep the program running
     while True:
